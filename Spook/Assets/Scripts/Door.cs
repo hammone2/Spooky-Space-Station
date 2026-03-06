d2061aa47a81c61cb73d3b233edec7cd87a4f502
hammone2 <hammone2@my.erau.edu>
2026-03-05 17:32:18 -0700

    Added switch state function to door

--- Spook/Assets/Scripts/Door.cs
+++ Spook/Assets/Scripts/Door.cs
@@ -10,13 +10,25 @@ public class Door : MonoBehaviour
     }
     [SerializeField] private DoorStates doorState;
 
+    private Renderer renderer; //replace this later with light object ref
+    [SerializeField] private Material greenLight;
+    [SerializeField] private Material yellowLight;
+    [SerializeField] private Material redLight;
+
+
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+        SwitchState(doorState);
+    }
+
 
     public void Unlock()
     {
         if (doorState == DoorStates.Inaccessible)
             return;
-        
-        doorState = DoorStates.Unlocked;
+
+        SwitchState(DoorStates.Unlocked);
         Open();
     }
 
@@ -25,7 +37,7 @@ public class Door : MonoBehaviour
         if (doorState == DoorStates.Inaccessible)
             return;
 
-        doorState = DoorStates.Locked;
+        SwitchState(DoorStates.Locked);
         Close();
     }
 
@@ -43,5 +55,24 @@ public class Door : MonoBehaviour
             return;
 
         //animation + other stuff
+    }
+
+    private void SwitchState(DoorStates newState)
+    {
+        doorState = newState;
+        switch (doorState)
+        {
+            case DoorStates.Unlocked:
+                renderer.material = greenLight;
+                break;
+
+            case DoorStates.Locked:
+                renderer.material = yellowLight;
+                break;
+
+            case DoorStates.Inaccessible:
+                renderer.material = redLight;
+                break;
+        }
     }
 }
